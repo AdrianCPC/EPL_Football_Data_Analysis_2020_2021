35a3d11c86efb46bb372a38a3b87b29af1546c17
AdrianPC <adriancamilopc@gmail.com>
2024-02-21 21:10:45 -0500

    adding more analysis with figure

--- EPL_DA_Football.py
+++ EPL_DA_Football.py
@@ -66,3 +66,31 @@ np.size((epl_df['Nationality'].unique()))
 nationality = epl_df.groupby('Nationality').size().sort_values(ascending=False)
 nationality.head(10).plot(kind='bar', figsize=(12,6), color=sns.color_palette('magma'))
 # %%
+
+#Clubs with maximum players in their squad
+epl_df['Club'].value_counts().nlargest(5).plot(kind='bar', color=sns.color_palette('viridis'))
+# %%
+
+#Clubs with leastt players in their squad
+epl_df['Club'].value_counts().nsmallest(5).plot(kind='bar', color=sns.color_palette('viridis'))
+
+# %%
+
+#Players based on age group
+Under20 = epl_df[epl_df['Age'] <= 20]
+age20_25 = epl_df[(epl_df['Age'] > 20) & (epl_df['Age'] <= 25)]
+age25_30 = epl_df[(epl_df['Age'] > 25) & (epl_df['Age'] <= 30)]
+Above30 = epl_df[epl_df['Age'] > 30]
+
+x = np.array([Under20['Name'].count(), age20_25['Name'].count(),age25_30['Name'].count(),Above30['Name'].count()])
+mylabels = ['<=20','>20 & <=25','>25 & <=30', '>30']
+plt.title('Total players with Age Groups', fontsize=20)
+plt.pie(x, labels = mylabels, autopct='%.1f%%')
+plt.show()
+# %%
+
+#Total under 20 players in each club
+players_under_20 = epl_df[epl_df['Age'] < 20]
+players_under_20['Club'].value_counts().plot(kind='bar', color= sns.color_palette('cubehelix'))
+
+# %%
